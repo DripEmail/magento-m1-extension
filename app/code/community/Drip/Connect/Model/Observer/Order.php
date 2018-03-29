bb32088a5219adc66d51ba741b5d0522d8a81501
Viacheslav Kolosov <skolosov@robofirm.com>
2018-03-29 17:43:26 +0300

    DRIP-23 add order details

--- app/code/community/Drip/Connect/Model/Observer/Order.php
+++ app/code/community/Drip/Connect/Model/Observer/Order.php
@@ -29,16 +29,81 @@ class Drip_Connect_Model_Observer_Order
             return;
         }
 
+        $customer = $order->getCustomer();
+        if ($customer->getId()) {
+            $email = $customer->getEmail();
+        } else {
+            // quest checkout
+            $email = $order->getQuote()->getCustomerEmail();
+        }
+
         if ($order->getState() == Mage_Sales_Model_Order::STATE_NEW) {
             $response = Mage::getModel('drip_connect/ApiCalls_Helper_RecordAnEvent', array(
-                'email' => $order->getCustomer()->getEmail(),
+                'email' => $email,
                 'action' => Drip_Connect_Model_ApiCalls_Helper_RecordAnEvent::EVENT_ORDER_CREATED,
-                'properties' => array(
-                    'source' => 'magento'
-                ),
+                'properties' => $this->getOrderData($order),
             ))->call();
         }
 
         $order->setIsAlreadyProcessed(true);
     }
+
+    /**
+     * get order's data we want send to drip
+     *
+     * @param  Mage_Sales_Model_Order $order
+     * @return array
+     */
+    protected function getOrderData($order)
+    {
+        $data = array(
+            'source' => 'magento',
+            'amount' => $order->getGrandTotal(),
+            'tax' => $order->getTaxAmount(),
+            'fees' => $order->getShippingAmount(),
+            'discounts' => $order->getDiscountAmount(),
+            'currency' => $order->getQuote()->getQuoteCurrencyCode(),
+            'items_count' => $order->getTotalQtyOrdered(),
+            'order_id' => $order->getIncrementId(),
+            'order_status' => $order->getState(),
+            'line_items' => $this->getItemsGroups($order),
+        );
+
+        return $data;
+    }
+
+    /**
+     * get order's items as groups with equal attr values
+     *
+     * @param  Mage_Sales_Model_Order $order
+     * @return array
+     */
+    protected function getItemsGroups($order)
+    {
+        $data = array();
+        foreach ($order->getQuote()->getAllItems() as $item) {
+            $product = Mage::getModel('catalog/product')->load($item->getProduct()->getId());
+            $group = array(
+                'product_id' => $item->getProductId(),
+                'sku' => $item->getSku(),
+                'name' => $item->getName(),
+                'brand' => $item->getBrand(),
+                'categories' => implode(',', $product->getCategoryIds()),
+                'quantity' => $item->getQty(),
+                'price' => $item->getPrice(),
+                'amount' => ($item->getQty() * $item->getPrice()),
+                'tax' => $item->getTaxAmount(),
+                'taxable' => (preg_match('/[123456789]/', $item->getTaxAmount()) ? 'true' : 'false'),
+                'discounts' => $item->getDiscountAmount(),
+                'discount_codes' => $order->getQuote()->getCouponCode(),
+                'currency' => $order->getQuote()->getQuoteCurrencyCode(),
+                'product_url' => $item->getProduct()->getProductUrl(),
+                'image_url' => (string)Mage::helper('catalog/image')->init($product, 'image'),
+            );
+            $data[] = $group;
+        }
+
+        return $data;
+    }
+
 }
