5c4dda007204d95848dd7f9655215f27c07d7b15
Viacheslav Kolosov <skolosov@robofirm.com>
2018-03-29 12:09:44 +0300

    DRIP-23 refactor account change to check the difference

--- app/code/community/Drip/Connect/Model/Observer/Account.php
+++ app/code/community/Drip/Connect/Model/Observer/Account.php
@@ -6,14 +6,24 @@
 class Drip_Connect_Model_Observer_Account
 {
     const REGISTRY_KEY_IS_NEW = 'newcustomer';
+    const REGISTRY_KEY_OLD_DATA = 'oldcustomerdata';
 
     /**
+     * - check if customer new
+     * - store old customer data (which is used in drip) to compare with later
+     *
      * @param Varien_Event_Observer $observer
      */
     public function checkIfCustomerNew($observer)
     {
         $customer = $observer->getCustomer();
         Mage::register(self::REGISTRY_KEY_IS_NEW, (bool)$customer->isObjectNew());
+
+        if (!$customer->isObjectNew()) {
+            $orig = Mage::getModel('customer/customer')->load($customer->getId());
+            $data = $this->prepareCustomerData($orig);
+            Mage::register(self::REGISTRY_KEY_OLD_DATA, $data);
+        }
     }
 
     /**
@@ -26,16 +36,19 @@ class Drip_Connect_Model_Observer_Account
         if (Mage::registry(self::REGISTRY_KEY_IS_NEW)) {
             $this->proceedAccountNew($customer);
         } else {
-            $this->proceedAccount($customer);
+            if ($this->isCustomerChanged($customer)) {
+                $this->proceedAccount($customer);
+            }
         }
         Mage::unregister(self::REGISTRY_KEY_IS_NEW);
+        Mage::unregister(self::REGISTRY_KEY_OLD_DATA);
     }
 
     /**
-     * @param Varien_Event_Observer $observer
-     *
      * change address from admin area get processed in afterCustomerSave() method
      * this one used for user's actions with address on front
+     *
+     * @param Varien_Event_Observer $observer
      */
     public function afterCustomerAddressSaveFront($observer)
     {
@@ -63,32 +76,8 @@ class Drip_Connect_Model_Observer_Account
      */
     protected function proceedAccountNew($customer)
     {
-        $gender = $customer->getGender();
-        if ($gender == 1) {
-            $gender = 'Male';
-        } else if ($gender == 2) {
-            $gender = 'Female';
-        }
-        Mage::getModel('drip_connect/ApiCalls_Helper_CreateUpdateSubscriber', array(
-            'email' => $customer->getEmail(),
-            'user_id' => $customer->getEntityId(),
-            'ip_address' => Mage::helper('core/http')->getRemoteAddr(),
-            'custom_fields' => array(
-                'first_name' => $customer->getFirstname(),
-                'last_name' => $customer->getLastname(),
-                'birthday' => $customer->getDob(),
-                'gender' => $gender,
-                'city' => ($customer->getDefaultShippingAddress() ? $customer->getDefaultShippingAddress()->getCity() : ''),
-                'state' => ($customer->getDefaultShippingAddress() ? $customer->getDefaultShippingAddress()->getRegion() : ''),
-                'zip_code' => ($customer->getDefaultShippingAddress() ? $customer->getDefaultShippingAddress()->getPostcode() : ''),
-                'country' => ($customer->getDefaultShippingAddress() ? $customer->getDefaultShippingAddress()->getCountry() : ''),
-                'phone_number' => ($customer->getDefaultShippingAddress() ? $customer->getDefaultShippingAddress()->getTelephone() : ''),
-                'magento_account_created' => $customer->getCreatedAt(),
-                'magento_customer_group' => Mage::getModel('customer/group')->load($customer->getGroupId())->getCustomerGroupCode(),
-                'magento_store' => $customer->getStoreId(),
-                'accepts_marketing' => ($customer->getIsSubscribed() ? 'yes' : 'no'),
-            ),
-        ))->call();
+        $customerData = $this->prepareCustomerData($customer, false);
+        Mage::getModel('drip_connect/ApiCalls_Helper_CreateUpdateSubscriber', $customerData)->call();
 
         $response = Mage::getModel('drip_connect/ApiCalls_Helper_RecordAnEvent', array(
             'email' => $customer->getEmail(),
@@ -106,38 +95,9 @@ class Drip_Connect_Model_Observer_Account
      */
     protected function proceedAccount($customer)
     {
-        if ($customer->getData('email') != $customer->getOrigData('email')) {
-            $newEmail = $customer->getData('email');
-        } else {
-            $newEmail = '';
-        }
-        $gender = $customer->getGender();
-        if ($gender == 1) {
-            $gender = 'Male';
-        } else if ($gender == 2) {
-            $gender = 'Female';
-        }
-        Mage::getModel('drip_connect/ApiCalls_Helper_CreateUpdateSubscriber', array(
-            'email' => $customer->getOrigData('email'),
-            'new_email' => ($newEmail ? $newEmail : ''),
-            'user_id' => $customer->getEntityId(),
-            'ip_address' => Mage::helper('core/http')->getRemoteAddr(),
-            'custom_fields' => array(
-                'first_name' => $customer->getFirstname(),
-                'last_name' => $customer->getLastname(),
-                'birthday' => $customer->getDob(),
-                'gender' => $gender,
-                'city' => ($customer->getDefaultShippingAddress() ? $customer->getDefaultShippingAddress()->getCity() : ''),
-                'state' => ($customer->getDefaultShippingAddress() ? $customer->getDefaultShippingAddress()->getRegion() : ''),
-                'zip_code' => ($customer->getDefaultShippingAddress() ? $customer->getDefaultShippingAddress()->getPostcode() : ''),
-                'country' => ($customer->getDefaultShippingAddress() ? $customer->getDefaultShippingAddress()->getCountry() : ''),
-                'phone_number' => ($customer->getDefaultShippingAddress() ? $customer->getDefaultShippingAddress()->getTelephone() : ''),
-                'magento_account_created' => $customer->getCreatedAt(),
-                'magento_customer_group' => Mage::getModel('customer/group')->load($customer->getGroupId())->getCustomerGroupCode(),
-                'magento_store' => $customer->getStoreId(),
-                'accepts_marketing' => ($customer->getIsSubscribed() ? 'yes' : 'no'),
-            ),
-        ))->call();
+        $customerData = $this->prepareCustomerData($customer);
+
+        Mage::getModel('drip_connect/ApiCalls_Helper_CreateUpdateSubscriber', $customerData)->call();
 
         $response = Mage::getModel('drip_connect/ApiCalls_Helper_RecordAnEvent', array(
             'email' => $customer->getEmail(),
@@ -162,5 +122,67 @@ class Drip_Connect_Model_Observer_Account
                 'source' => 'magento'
             ),
         ))->call();
+    }
+
+    /**
+     * compare orig and new data
+     *
+     * @param Mage_Customer_Model_Customer $customer
+     */
+    protected function isCustomerChanged($customer)
+    {
+        $oldData = Mage::registry(self::REGISTRY_KEY_OLD_DATA);
+        $newData = $this->prepareCustomerData($customer);
+
+        return (serialize($oldData) != serialize($newData));
+    }
+
+    /**
+     * prepare array of customer data we use to send in drip
+     *
+     * @param Mage_Customer_Model_Customer $customer
+     * @param bool $updatableOnly leave only those fields which are used in update action
+     */
+    protected function prepareCustomerData($customer, $updatableOnly = true)
+    {
+        if ($customer->getOrigData() && $customer->getData('email') != $customer->getOrigData('email')) {
+            $newEmail = $customer->getData('email');
+        } else {
+            $newEmail = '';
+        }
+        $gender = $customer->getGender();
+        if ($gender == 1) {
+            $gender = 'Male';
+        } else if ($gender == 2) {
+            $gender = 'Female';
+        }
+        $data = array (
+            'email' => $customer->getEmail(),
+            'new_email' => ($newEmail ? $newEmail : ''),
+            'user_id' => $customer->getEntityId(),
+            'ip_address' => Mage::helper('core/http')->getRemoteAddr(),
+            'custom_fields' => array(
+                'first_name' => $customer->getFirstname(),
+                'last_name' => $customer->getLastname(),
+                'birthday' => $customer->getDob(),
+                'gender' => $gender,
+                'city' => ($customer->getDefaultShippingAddress() ? $customer->getDefaultShippingAddress()->getCity() : ''),
+                'state' => ($customer->getDefaultShippingAddress() ? $customer->getDefaultShippingAddress()->getRegion() : ''),
+                'zip_code' => ($customer->getDefaultShippingAddress() ? $customer->getDefaultShippingAddress()->getPostcode() : ''),
+                'country' => ($customer->getDefaultShippingAddress() ? $customer->getDefaultShippingAddress()->getCountry() : ''),
+                'phone_number' => ($customer->getDefaultShippingAddress() ? $customer->getDefaultShippingAddress()->getTelephone() : ''),
+                'magento_account_created' => $customer->getCreatedAt(),
+                'magento_customer_group' => Mage::getModel('customer/group')->load($customer->getGroupId())->getCustomerGroupCode(),
+                'magento_store' => $customer->getStoreId(),
+                'accepts_marketing' => ($customer->getIsSubscribed() ? 'yes' : 'no'),
+            ),
+        );
+
+        if ($updatableOnly) {
+            unset($data['custom_fields']['magento_account_created']);
+            unset($data['custom_fields']['ip_address']);
+        }
+
+        return $data;
     }
 }
