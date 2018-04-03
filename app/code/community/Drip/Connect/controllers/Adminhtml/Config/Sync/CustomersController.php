2026c000b6edfc48f454fc1f58fc168577657258
Viacheslav Kolosov <skolosov@robofirm.com>
2018-04-03 13:19:13 +0300

    DRIP-31 send batch

--- app/code/community/Drip/Connect/controllers/Adminhtml/Config/Sync/CustomersController.php
+++ app/code/community/Drip/Connect/controllers/Adminhtml/Config/Sync/CustomersController.php
@@ -11,7 +11,32 @@ class Drip_Connect_Adminhtml_Config_Sync_CustomersController
     public function runAction()
     {
         $accountId = $this->getRequest()->getParam('account_id');
+        $result = 1;
+        $page = 1;
+        do {
+            $collection = Mage::getModel('customer/customer')
+                ->getCollection()
+                ->addAttributeToSelect('*')
+                ->setPageSize(Drip_Connect_Model_ApiCalls_Helper::MAX_BATCH_SIZE)
+                ->setCurPage($page++)
+                ->load();
 
-        //$this->getResponse()->setBody(1);
+            $batch = array();
+            foreach ($collection as $customer) {
+                $batch[] = Drip_Connect_Helper_Data::prepareCustomerData($customer);
+            }
+
+            $response = Mage::getModel('drip_connect/ApiCalls_Helper_Batches_Subscribers', array(
+                'batch' => $batch,
+                'account' => $accountId,
+            ))->call();
+
+            if ($response->getResponseCode() != 201) {
+                $result = 0;
+                break;
+            }
+        } while ($page <= $collection->getLastPageNumber());
+
+        $this->getResponse()->setBody($result);
     }
 }
