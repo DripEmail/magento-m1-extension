63d2e1648ac78564e9b29aacb0b1ddccb5cbd0b1
Viacheslav Kolosov <skolosov@robofirm.com>
2018-04-06 17:53:14 +0300

    DRIP-32 order batch api call

---
+++ app/code/community/Drip/Connect/Model/ApiCalls/Helper/Batches/Orders.php
@@ -0,0 +1,30 @@
+<?php
+
+class Drip_Connect_Model_ApiCalls_Helper_Batches_Orders
+    extends Drip_Connect_Model_ApiCalls_Helper
+{
+    public function __construct($data = null)
+    {
+        if (empty($data['account'])) {
+            $accountId = Mage::getStoreConfig('dripconnect_general/api_settings/account_id');
+        } else {
+            $accountId = (int)$data['account'];
+        }
+        $this->apiClient = Mage::getModel('drip_connect/ApiCalls_Base', array(
+            'endpoint' => $accountId.'/'.self::ENDPOINT_BATCH_ORDERS,
+        ));
+
+        $ordersInfo = [
+            'orders' => $data['batch']
+        ];
+        $batchesInfo = [
+            'batches' => [
+                $ordersInfo
+            ]
+        ];
+
+        $this->request = Mage::getModel('drip_connect/ApiCalls_Request_Base')
+            ->setMethod(Zend_Http_Client::POST)
+            ->setRawData(json_encode($batchesInfo));
+    }
+}
--- app/code/community/Drip/Connect/controllers/Adminhtml/Config/Sync/OrdersController.php
+++ app/code/community/Drip/Connect/controllers/Adminhtml/Config/Sync/OrdersController.php
@@ -14,20 +14,28 @@ class Drip_Connect_Adminhtml_Config_Sync_OrdersController
         $result = 1;
         $page = 1;
         do {
-            // todo filder orders
             $collection = Mage::getModel('sales/order')
                 ->getCollection()
                 ->addAttributeToSelect('*')
+                ->addFieldToFilter('state', array('nin' => array(
+                    Mage_Sales_Model_Order::STATE_CANCELED,
+                    Mage_Sales_Model_Order::STATE_CLOSED
+                    )))
                 ->setPageSize(Drip_Connect_Model_ApiCalls_Helper::MAX_BATCH_SIZE)
                 ->setCurPage($page++)
                 ->load();
 
             $batch = array();
             foreach ($collection as $order) {
-                //$batch[] = Drip_Connect_Helper_Data::prepareOrderData($order);
+                $data = Mage::helper('drip_connect/order')->getOrderDataNew($order);
+                $data['occurred_at'] = $order->getCreatedAt();
+                $batch[] = $data;
             }
 
-            // todo API call
+            $response = Mage::getModel('drip_connect/ApiCalls_Helper_Batches_Orders', array(
+                'batch' => $batch,
+                'account' => $accountId,
+            ))->call();
 
             if ($response->getResponseCode() != 202) { // drip success code for this action
                 $result = 0;
