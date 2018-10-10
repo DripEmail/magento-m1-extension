f473bdf14a17a5286581bd11da02f0d431ff09e3
Viacheslav Kolosov <skolosov@robofirm.com>
2018-10-10 13:30:21 +0300

    clear registry before use

--- app/code/community/Drip/Connect/Model/Observer/Quote.php
+++ app/code/community/Drip/Connect/Model/Observer/Quote.php
@@ -23,12 +23,14 @@ class Drip_Connect_Model_Observer_Quote
         if (!$quote->isObjectNew()) {
             $orig = Mage::getModel('sales/quote')->load($quote->getId());
             $data = Mage::helper('drip_connect/quote')->prepareQuoteData($orig);
+            Mage::unregister(Drip_Connect_Helper_Quote::REGISTRY_KEY_OLD_DATA);
             Mage::register(Drip_Connect_Helper_Quote::REGISTRY_KEY_OLD_DATA, $data);
         } else {
             Mage::helper('drip_connect/quote')->checkForEmptyQuote($quote);
         }
 
         if (!Mage::registry(Drip_Connect_Helper_Quote::REGISTRY_KEY_CUSTOMER_REGISTERED_OR_LOGGED_IN_WITH_EMTPY_QUOTE)) {
+            Mage::unregister(Drip_Connect_Helper_Quote::REGISTRY_KEY_IS_NEW);
             if (!$quote->getDrip()) {
                 Mage::register(Drip_Connect_Helper_Quote::REGISTRY_KEY_IS_NEW, true);
                 $quote->setDrip(true);
