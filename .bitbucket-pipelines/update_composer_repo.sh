32ee07b08db2101112a5354d81957ea6f6ee5d36
Matthew Feinberg <matthew.feinberg@rmgmedia.com>
2019-07-17 15:32:40 +0000

    ROBO2RMG-60 changed urls for robo2rmg

--- .bitbucket-pipelines/update_composer_repo.sh
+++ .bitbucket-pipelines/update_composer_repo.sh
@@ -9,7 +9,7 @@ fi
 (umask  077 ; echo $COMPOSER_KNOWN_HOST | base64 --decode >> ~/.ssh/known_hosts);
 
 # Run Satis on Robofirm Composer
-ssh -q webuser@composer.robofirm.net "
+ssh -q webuser@composer.rmgmedia.com "
     cd /var/www/composer &&
     php vendor/composer/satis/bin/satis build magento-module.json htdocs/magento/module robofirm/drip_m1connect
 "
