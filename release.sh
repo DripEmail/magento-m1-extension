e2bac78385f4b23c7bd43cf8489af87785adf96d
William Johnston <william.johnston@drip.com>
2019-08-21 10:49:47 -0500

    Update release script

--- release.sh
+++ release.sh
@@ -10,28 +10,26 @@ if (! git diff-index --quiet HEAD --); then
   exit
 fi
 
-# Get release branch version.
-current_branch=$(git branch --show-current)
-if [[ "$current_branch" = 'master' ]]; then
-  echo "You should release from a release branch, not master. Exiting..."
-  exit
-fi
-branch_version=$(echo -n "$current_branch" | cut -d'-' -f2)
-echo "Detected branch version is $branch_version"
+# Get expected version.
+echo "What version are you releasing?"
+read release_version
 
 # Check for XML versions
-if (! grep "<version>$branch_version</version>" $CONFIG_XML_PATH > /dev/null); then
+if (! grep "<version>$release_version</version>" $CONFIG_XML_PATH > /dev/null); then
   echo "$CONFIG_XML_PATH does not contain the right version. Exiting..."
   exit
 fi
 
-if (! grep "<version>$branch_version</version>" $PACKAGE_XML_PATH > /dev/null); then
+if (! grep "<version>$release_version</version>" $PACKAGE_XML_PATH > /dev/null); then
   echo "$PACKAGE_XML_PATH does not contain the right version. Exiting..."
   exit
 fi
 
+echo "Did you regenerate package.xml? If not, cancel and do so."
+read
+
 echo "All versions check out. Generating tarball..."
 
-git archive --format=tar HEAD | gzip - > drip_m1connect-$(echo -n $branch_version | tr '.' '_')-$(date "+%Y-%m-%d").tgz
+git archive --format=tar HEAD | gzip - > drip_m1connect-$(echo -n $release_version | tr '.' '_')-$(date "+%Y-%m-%d").tgz
 
 echo "Tarball generated. Don't forget to tag a release and push the tag."
