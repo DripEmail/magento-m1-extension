b2bc9a5075efd5c0f88860c0f12ad55116f4aa02
William Johnston <william.johnston@drip.com>
2019-09-12 10:04:52 -0500

    Properly handle customer update

--- app/code/community/Drip/Connect/Model/Observer/Customer/AfterSave.php
+++ app/code/community/Drip/Connect/Model/Observer/Customer/AfterSave.php
@@ -13,7 +13,12 @@ class Drip_Connect_Model_Observer_Customer_AfterSave extends Drip_Connect_Model_
             Mage::helper('drip_connect')->proceedAccount($customer, null, Drip_Connect_Model_ApiCalls_Helper_RecordAnEvent::EVENT_CUSTOMER_NEW);
         } else {
             if ($this->isCustomerChanged($customer)) {
-                Mage::helper('drip_connect')->proceedAccount($customer);
+                Mage::helper('drip_connect/customer')->proceedAccount(
+                    $customer,
+                    Mage::registry(self::REGISTRY_KEY_SUBSCRIBER_SUBSCRIBE_INTENT),
+                    Drip_Connect_Model_ApiCalls_Helper_RecordAnEvent::EVENT_CUSTOMER_UPDATED,
+                    $this->isCustomerStatusChanged($customer)
+                );
             }
         }
         Mage::unregister(self::REGISTRY_KEY_CUSTOMER_IS_NEW);
@@ -31,5 +36,18 @@ class Drip_Connect_Model_Observer_Customer_AfterSave extends Drip_Connect_Model_
         $newData = Drip_Connect_Helper_Data::prepareCustomerData($customer);
 
         return (Mage::helper('core')->jsonEncode($oldData) != Mage::helper('core')->jsonEncode($newData));
+    }
+
+    protected function isCustomerStatusChanged($customer)
+    {
+        $newStatus = Mage::registry(self::REGISTRY_KEY_SUBSCRIBER_SUBSCRIBE_INTENT);
+        if ($newStatus === null) {
+            return false;
+        }
+
+        $oldData = Mage::registry(self::REGISTRY_KEY_CUSTOMER_OLD_DATA);
+        // TODO: Refactor away stringly typed boolean.
+        $oldStatus = $oldData['custom_fields']['accepts_marketing'] == 'yes';
+        return $oldStatus !== $newStatus;
     }
 }
