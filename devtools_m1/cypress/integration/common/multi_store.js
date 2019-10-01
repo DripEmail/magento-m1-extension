84bf1ef840453938633f076c54cf2730c385ba31
William Johnston <william.johnston@drip.com>
2019-10-01 09:46:36 -0500

    Cart interaction tests

--- devtools_m1/cypress/integration/common/multi_store.js
+++ devtools_m1/cypress/integration/common/multi_store.js
@@ -7,15 +7,3 @@ When('I open the site1 homepage', function() {
 When('I open the main homepage', function() {
   cy.visit(`http://main.magento.localhost:3005/`)
 })
-
-Then('clientjs is inserted', function() {
-  cy.window().then(function(win) {
-    expect(win._dcq).to.not.be.undefined
-  })
-})
-
-Then('clientjs is not inserted', function() {
-  cy.window().then(function(win) {
-    expect(win._dcq).to.be.undefined
-  })
-})
