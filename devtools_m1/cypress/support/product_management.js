2790915c161fc247f83845cf561aedac5691ce12
William Johnston <william.johnston@drip.com>
2019-10-17 13:40:13 -0500

    Add test for admin order creation

--- devtools_m1/cypress/support/product_management.js
+++ devtools_m1/cypress/support/product_management.js
@@ -13,3 +13,13 @@ Cypress.Commands.add("createProduct", (desc) => {
     }
   })
 })
+
+Cypress.Commands.add("createCustomer", (desc) => {
+  cy.log('Creating magento customer')
+  const str = JSON.stringify(desc)
+  cy.exec(`echo '${str}' | ./docker_compose.sh exec -T web /bin/php -f shell/drip/create_customer.php`, {
+    env: {
+      DRIP_COMPOSE_ENV: 'test'
+    }
+  })
+})
