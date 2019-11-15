02bb5399be90dcc1ce5d3005db73310320b6e10a
Thom Dieterich <thom.dieterich@drip.com>
2019-11-15 15:49:36 -0600

    Greenbar checkin: created tests around wishlist modifaction fixed helper calls to use the correct helper
    Adding, removing via quanity, and removing via the trashcan all generate an event as expected

--- app/code/community/Drip/Connect/Model/Observer/Wishlist/AddProduct.php
+++ app/code/community/Drip/Connect/Model/Observer/Wishlist/AddProduct.php
@@ -11,7 +11,7 @@ class Drip_Connect_Model_Observer_Wishlist_AddProduct extends Drip_Connect_Model
         $customer = Mage::getSingleton('customer/session')->getCustomer();
         $product = $observer->getProduct();
 
-        Mage::helper('drip_connect')->doWishlistEvent(
+        Mage::helper('drip_connect/wishlist')->doWishlistEvent(
             Drip_Connect_Model_ApiCalls_Helper_RecordAnEvent::EVENT_WISHLIST_ADD_PRODUCT,
             $customer,
             $product
--- app/code/community/Drip/Connect/Model/Observer/Wishlist/PredispatchWishlistIndexRemove.php
+++ app/code/community/Drip/Connect/Model/Observer/Wishlist/PredispatchWishlistIndexRemove.php
@@ -14,7 +14,7 @@ class Drip_Connect_Model_Observer_Wishlist_PredispatchWishlistIndexRemove extend
             $product = Mage::getModel('catalog/product')->load($wishlistItem->getProductId());
             $customer = Mage::getSingleton('customer/session')->getCustomer();
 
-            Mage::helper('drip_connect')->doWishlistEvent(
+            Mage::helper('drip_connect/wishlist')->doWishlistEvent(
                 Drip_Connect_Model_ApiCalls_Helper_RecordAnEvent::EVENT_WISHLIST_REMOVE_PRODUCT,
                 $customer,
                 $product
