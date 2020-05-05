9ab8e7a13b7bf4731f634b3b7a29ef22020a61cb
William Johnston <william.johnston@drip.com>
2020-05-05 10:28:09 -0500

    Finally set the default to the only value we actually use

--- devtools_m1/docker_compose.sh
+++ devtools_m1/docker_compose.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 
-DRIP_COMPOSE_ENV1=${DRIP_COMPOSE_ENV:-"dev"}
+DRIP_COMPOSE_ENV1=${DRIP_COMPOSE_ENV:-"test"}
 
 docker-compose -p "devtools_m1_${DRIP_COMPOSE_ENV1}" -f docker-compose.base.yml -f "docker-compose.${DRIP_COMPOSE_ENV1}.yml" "$@"
